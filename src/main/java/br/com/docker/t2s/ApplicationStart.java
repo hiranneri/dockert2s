2954069033ac8d76ff722705bb293f63c5adbba7
hiranneri <hiran.souza93@gmail.com>
2023-11-21 17:46:17 -0300

    Inclusão de observabilidade no projeto
    Inclusão do Spring Actuator;
    Inclusão do Prometheus
    Inclusão do Grafana

--- src/main/java/br/com/docker/t2s/ApplicationStart.java
+++ src/main/java/br/com/docker/t2s/ApplicationStart.java
@@ -1,9 +1,13 @@
 package br.com.docker.t2s;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -16,5 +20,11 @@ public class ApplicationStart {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 }
