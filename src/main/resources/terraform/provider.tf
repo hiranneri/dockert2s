b9e71899479499a0fb48d08820dcbfe43890f71f
hiranneri <hiran.souza93@gmail.com>
2023-12-08 22:50:18 -0300

    Inclusão do terraform e configs adicionais do Docker

---
+++ src/main/resources/terraform/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
