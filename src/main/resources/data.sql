91a9f4b76a76237e8c006b4559db6907cb248a7e
hiranneri <hiran.souza93@gmail.com>
2023-12-13 01:18:07 -0300

    Inclusão de variáveis de ambiente e Mudança de banco de dados para o Postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,27 @@
-INSERT IGNORE INTO tiposmovimentacao  (nome, status)
-SELECT 'DESCARGA', 1
-UNION
-SELECT 'EMBARQUE', 1
-UNION
-SELECT 'GATE_IN', 1
-UNION
-SELECT 'GATE_OUT', 1
-UNION
-SELECT 'PESAGEM', 1
-UNION
-SELECT 'PILHA', 1
-UNION
-SELECT 'POSICIONAMENTO', 1
-UNION
-SELECT 'SCANNER', 1
-FROM dual
-WHERE NOT EXISTS (SELECT 1 FROM tiposmovimentacao);
-
-INSERT INTO usuarios
-SELECT *
-FROM (
-  VALUES
-  ROW(NULL,'ROLE_ADMIN', 'HIRAN', '$2a$10$tlFWUcQeXmqMSnvLu1j6meKNZG3yyiBTUtv21HrNJSPDZx7XLTIXG', 1,'hiran'),
-  ROW(NULL , 'ROLE_USER', 'ADMIN', '$2a$10$tlFWUcQeXmqMSnvLu1j6meKNZG3yyiBTUtv21HrNJSPDZx7XLTIXG',1, 'admin')
-) source_data
+INSERT INTO tiposmovimentacao (nome, status)
+SELECT nome, status
+FROM (VALUES
+  ('DESCARGA',true),
+  ('EMBARQUE',true),
+  ('GATE_IN', true),
+  ( 'GATE_OUT', true),
+  ( 'PESAGEM', true),
+  ( 'PILHA', true),
+  ( 'POSICIONAMENTO', true),
+  ( 'SCANNER', true)
+) AS tipos (nome,status)
 WHERE NOT EXISTS (
-  SELECT NULL
+  SELECT 1
+  FROM tiposmovimentacao
+);
+
+INSERT INTO usuarios (authorities,name,password,status,username)
+SELECT authorities,name,password,status,username
+FROM (VALUES
+  ('ROLE_ADMIN', 'HIRAN', '$2a$10$tlFWUcQeXmqMSnvLu1j6meKNZG3yyiBTUtv21HrNJSPDZx7XLTIXG', true,'hiran'),
+  ('ROLE_USER', 'ADMIN', '$2a$10$tlFWUcQeXmqMSnvLu1j6meKNZG3yyiBTUtv21HrNJSPDZx7XLTIXG',true, 'admin')
+) AS usuarios (authorities,name,password,status,username)
+WHERE NOT EXISTS (
+  SELECT 1
   FROM usuarios
 );
--- src/test/java/br/com/docker/t2s/integration/TestContainerConfig.java
+++ src/test/java/br/com/docker/t2s/integration/TestContainerConfig.java
@@ -3,7 +3,7 @@ package br.com.docker.t2s.integration;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -11,19 +11,19 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public class TestContainerConfig implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     @Container
-    public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:latest")
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15.4-alpine")
             .withDatabaseName("dockert2s-test")
-            .withPassword("secret")
+            .withPassword("secretpg")
             .withReuse(true);
 
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-        mySQLContainer.start();
+         postgreSQLContainer.start();
         TestPropertyValues.of(
-                "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
-                "spring.datasource.username=" + mySQLContainer.getUsername(),
-                "spring.datasource.password=" + mySQLContainer.getPassword(),
-                "spring.datasource.driver-class-name" + mySQLContainer.getDriverClassName()
+                "spring.datasource.url=" +  postgreSQLContainer.getJdbcUrl(),
+                "spring.datasource.username=" +  postgreSQLContainer.getUsername(),
+                "spring.datasource.password=" +  postgreSQLContainer.getPassword(),
+                "spring.datasource.driver-class-name" +  postgreSQLContainer.getDriverClassName()
         ).applyTo(applicationContext.getEnvironment());
     }
 
